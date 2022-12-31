b3512a2be6060990355951aafcbf5a781cb22c5e
unknown <r.david000@gmail.com>
2022-12-31 18:05:19 +0100

    my

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
+wget https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-11.6.0-amd64-netinst.iso
 apt-get update
-apt-get install -y -q apache2 php php-curl sudo rsync genisoimage 
+apt-get install -y -q apache2 php php-curl sudo rsync genisoimage
 echo 'www-data        ALL=(ALL)       NOPASSWD: ALL' >> /etc/sudoers
 rm /var/www/index.html
 mv * /var/www/
