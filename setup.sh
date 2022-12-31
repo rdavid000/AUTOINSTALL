ad8d0a3f41a85a821d3727bed13af13abcd71433
unknown <r.david000@gmail.com>
2022-12-31 19:13:35 +0100

    my

--- setup.sh
+++ setup.sh
@@ -3,10 +3,8 @@ export DEBIAN_FRONTEND=noninteractive
 wget https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-11.6.0-amd64-netinst.iso
 apt-get update
 apt-get install -y -q apache2 php php-curl sudo rsync genisoimage
-echo 'www-data        ALL=(ALL)       NOPASSWD: ALL' >> /etc/sudoers
 rm /var/www/index.html
-mv * /var/www/
-chmod 777 /var/www -R
+echo 'www-data        ALL=(ALL)       NOPASSWD: ALL' >> /etc/sudoers
 service sudo restart
 service apache2 restart
 exit 1
