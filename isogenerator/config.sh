1f12aac78a94b1f166d78dbc8cbe425ac04b1c46
unknown <r.david000@gmail.com>
2023-01-01 04:50:39 +0100

    my

--- isogenerator/config.sh
+++ isogenerator/config.sh
@@ -6,3 +6,7 @@ __SYS_PASSWORD__='1234'
 __SYS_PRESEED__="cp /preseed.cfg /target/root; wget -O /target/etc/systemd/system/FIRSTBOOT.service https://raw.githubusercontent.com/rdavid000/AUTOINSTALL/main/startup/FIRSTBOOT.service; wget -O /target/root/FIRSTBOOT.sh https://raw.githubusercontent.com/rdavid000/AUTOINSTALL/main/startup/FIRSTBOOT.sh; chroot /target chmod +x /etc/systemd/system/FIRSTBOOT.service; chroot /target chmod +x /root/FIRSTBOOT.sh; systemctl enable FIRSTBOOT.service;"
 __INS_FIRMWARE__='http://r.david000.free.fr/firmware-realtek_0.36_all.deb'
 __INS_ISO__='debian-11.6.0-amd64-netinst'
+__NET_IP__='192.168.200.203'
+__NET_MASK__='255.255.255.0'
+__NET_GW__='192.168.200.1'
+__NET_DNS__='192.168.200.1'
