83c58af79e107d82175f0c637c401aa5d3343555
unknown <r.david000@gmail.com>
2023-01-01 04:43:43 +0100

    my

--- isogenerator/config.sh
+++ isogenerator/config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-__SYS_HOSTNAME__='MYROBOT'
-__SYS_DOMAIN__='WORKGROUP'
+__SYS_HOSTNAME__='IGetPreseed'
+__SYS_DOMAIN__=''
 __SYS_PROXY__=''
 __SYS_PASSWORD__='1234'
-__SYS_PRESEED__="cp /preseed.cfg /target/root; wget -O /target/etc/systemd/system/FIRSTBOOT.service https://raw.githubusercontent.com/rdavid000/AUTOINSTALL/main/startup/FIRSTBOOT.service; wget -O /target/root/FIRSTBOOT.sh https://raw.githubusercontent.com/rdavid000/AUTOINSTALL/main/startup/FIRSTBOOT.sh; chroot /target chmod +x /etc/systemd/system/FIRSTBOOT.service; chroot /target chmod +x /root/FIRSTBOOT.sh;systemctl enable FIRSTBOOT.service"
+__SYS_PRESEED__="cp /preseed.cfg /target/root; wget -O /target/etc/systemd/system/FIRSTBOOT.service https://raw.githubusercontent.com/rdavid000/AUTOINSTALL/main/startup/FIRSTBOOT.service; wget -O /target/root/FIRSTBOOT.sh https://raw.githubusercontent.com/rdavid000/AUTOINSTALL/main/startup/FIRSTBOOT.sh; chroot /target chmod +x /etc/systemd/system/FIRSTBOOT.service; chroot /target chmod +x /root/FIRSTBOOT.sh; systemctl enable FIRSTBOOT.service;"
 __INS_FIRMWARE__='http://r.david000.free.fr/firmware-realtek_0.36_all.deb'
 __INS_ISO__='debian-11.6.0-amd64-netinst'
