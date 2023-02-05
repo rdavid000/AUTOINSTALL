9c1ebea6fedab2ac19f7d6c5dfdb7771a957917b
Pharah <r.david000@gmail.com>
2023-02-06 00:06:28 +0100

    my

--- docker/start.sh
+++ docker/start.sh
@@ -6,8 +6,8 @@ cd /root/ && git clone https://github.com/rdavid000/AUTOINSTALL.git
 cd AUTOINSTALL/iso && wget -O debian-11.6.0-amd64-netinst.iso http://miroir.univ-lorraine.fr/debian-cd/11.6.0/amd64/iso-cd/debian-11.6.0-amd64-netinst.iso
 cd ../isogenerator && chmod +x ./* -R
 ./generator.sh
-echo -e "\n\nYour /root/AUTOINSTALL/isogenerator/cutom_debian-11.6.0-amd64-netinst.iso is ready \nYou can run ./generator.sh again if you have to modify isogenerator/config.sh\n"
-mv /root/custom* /home/r2d2/
+mv /root/AUTOINSTALL/isogenerator/custom* /home/r2d2/
 cd /home/r2d2/
 chmod 777 `ls custom_*`
+echo -e "\n\nYour /home/r2d2/cutom_debian-11.6.0-amd64-netinst.iso is ready \nYou can run ./generator.sh again if you have to modify isogenerator/config.sh\n"
 exit 0
