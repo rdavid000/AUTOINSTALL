279e10958aa0e2b29c6cd513350b32f9cb0f3788
Pharah <r.david000@gmail.com>
2023-02-06 13:05:50 +0100

    my

--- startup/FIRSTBOOT.sh
+++ startup/FIRSTBOOT.sh
@@ -26,6 +26,6 @@ newgrp docker
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
 docker build https://github.com/rdavid000/MYKALI.git#main:docker -t mykali 2> mykali_build.log
-sudo docker run --name MYKALI --restart=always -p 4444:22 -p 4445:8080 mykali
+sudo docker run --name MYKALI --tty -d --restart unless-stopped -p 4444:22 -p 4445:8080 mykali
 touch /root/FIRSTBOOT_END
 exit 0
