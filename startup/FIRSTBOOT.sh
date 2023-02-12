a2f60c5f361e21d9b0b90c10066027f0d4f5c9a5
Pharah <r.david000@gmail.com>
2023-02-12 12:25:04 +0100

    my

--- startup/FIRSTBOOT.sh
+++ startup/FIRSTBOOT.sh
@@ -15,7 +15,7 @@ apt-get clean -yq
 systemctl daemon-reload
 sudo su r2d2
 cd /home/r2d2
-sudo apt install ca-certificates curl gnupg lsb-release -y
+sudo apt install ca-certificates curl wget gnupg lsb-release -y
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
@@ -25,7 +25,7 @@ sudo usermod -aG docker r2d2
 newgrp docker
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
-docker build https://github.com/rdavid000/MYKALI.git#main:docker -t mykali 2> mykali_build.log --no-cache
+docker build --no-cache https://github.com/rdavid000/MYKALI.git#main:docker -t mykali 2> mykali_build.log
 sudo docker run --name MYKALI --tty --privileged -d --restart unless-stopped -p 4444:22 -p 4445:8080 mykali
 #curl https://raw.githubusercontent.com/rdavid000/Exegol/main/startup/firstboot.sh -o exegol.sh
 #chmod +x exegol.sh
