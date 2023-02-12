405ea735120f92077409ec6084769ffa21d368a6
Pharah <r.david000@gmail.com>
2023-02-12 12:28:22 +0100

    my

--- startup/FIRSTBOOT.sh
+++ startup/FIRSTBOOT.sh
@@ -15,7 +15,7 @@ apt-get clean -yq
 systemctl daemon-reload
 sudo su r2d2
 cd /home/r2d2
-sudo apt install ca-certificates curl wget gnupg lsb-release -y
+sudo apt install ca-certificates curl gnupg lsb-release -y
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
